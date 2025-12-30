1cfea7c76d66b880b88798eebae79d26f40091c4
Sairam Somaraju <sairamso@infinite.com>
2025-12-30 23:27:18 +0530

    first commit

---
+++ ASP.Net/Practice/Navigations_Prj/CrossPage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class CrossPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(PreviousPage!=null && PreviousPage.IsCrossPagePostBack)
+            {
+                TextBox txtname =(TextBox) PreviousPage.FindControl("txtName");
+                Label1.Text = "Welcome " + txtname.Text;
+            }
+            else
+            {
+                Response.Redirect("CrossPageIndex.aspx");
+            }
+        }
+    }
+}
